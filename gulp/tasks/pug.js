3a0e795f1021dc8d2c8b753de46e3f18238caddf
Ion Talmaci <ntalmaci171@gmail.com>
2019-01-23 15:18:37 +0200

    fixing

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -4,8 +4,7 @@ module.exports = function() {
     $.gulp.task('pug', function(){
         return $.gulp.src($.config.src + '/template/pages/*.pug')
         .pipe($.gp.pug({
-            locals : JSON.parse($.fs.readFileSync('./content.json', 'utf8')),
-            pretty : true,
+            pretty : true
         }))
         .on('error', $.gp.notify.onError(function(error){
             return {
