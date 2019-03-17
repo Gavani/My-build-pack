c6fbd5b101ef724c3316bd15c007c9d47eaec91c
Ion Talmaci <ntalmaci171@gmail.com>
2019-03-17 16:28:45 +0200

    Revert to

--- gulp/tasks/sprite.svg.js
+++ gulp/tasks/sprite.svg.js
@@ -3,6 +3,27 @@
 module.exports = function() {
     $.gulp.task('svg', () => {
         return $.gulp.src($.config.src +'/img/icons/*.svg')
+            .pipe($.gp.svgmin({
+                js2svg: {
+                    pretty: true
+                }
+            }))
+            .pipe($.gp.cheerio({
+                run: function($) {
+                    $('[fill]').removeAttr('fill');
+                    $('[stroke]').removeAttr('stroke');
+                    $('[style]').removeAttr('style');
+                },
+                parserOptions: { xmlMode: true }
+            }))
+            .pipe($.gp.replace('&gt;', '>'))
+            .pipe($.gp.svgSprite({
+                mode: {
+                    symbol: {
+                        sprite: "sprite.svg"
+                    }
+                }
+            }))
             .pipe($.gulp.dest($.config.build +'/assets/img/icons/'));
     });
 };
